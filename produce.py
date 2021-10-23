4d83517e504f6a1eee0b57278cb4f1a962520cd2
EGOISTK21 <egoistk21@gmail.com>
2021-10-24 00:33:04 +0800

    change to real ip mode

--- produce.py
+++ produce.py
@@ -42,23 +42,36 @@ def dump_bird(lst, f):
 RESERVED = [
     IPv4Network('0.0.0.0/8'),
     IPv4Network('10.0.0.0/8'),
+    IPv4Network('100.64.0.0/10'),
     IPv4Network('127.0.0.0/8'),
     IPv4Network('169.254.0.0/16'),
     IPv4Network('172.16.0.0/12'),
-    IPv4Network('192.0.0.0/29'),
-    IPv4Network('192.0.0.170/31'),
+    IPv4Network('192.0.0.0/24'),
     IPv4Network('192.0.2.0/24'),
+    IPv4Network('192.88.99.0/24'),
     IPv4Network('192.168.0.0/16'),
     IPv4Network('198.18.0.0/15'),
     IPv4Network('198.51.100.0/24'),
     IPv4Network('203.0.113.0/24'),
+    IPv4Network('223.255.255.0/24'),
+    IPv4Network('224.0.0.0/4'),
     IPv4Network('240.0.0.0/4'),
     IPv4Network('255.255.255.255/32'),
-    IPv4Network('224.0.0.0/4'),
-    IPv4Network('100.64.0.0/10'),
 ]
 RESERVED_V6 = [
+    IPv6Network('::/128'),
+    IPv6Network('::1/128'),
+    IPv6Network('::ffff:0:0/96'),
+    IPv6Network('100::/64'),
+    IPv6Network('64:ff9b::/96'),
+    IPv6Network('2001::/32'),
+    IPv6Network('2001:10::/28'),
+    IPv6Network('2001:20::/28'),
+    IPv6Network('2001:db8::/32'),
+    IPv6Network('2002::/16'),
+    IPv6Network('fc00::/7'),
     IPv6Network('fe80::/10'),
+    IPv6Network('ff00::/8'),
 ]
 if args.exclude:
     for e in args.exclude:
@@ -125,7 +138,7 @@ subtract_cidr(root, RESERVED)
 subtract_cidr(root_v6, RESERVED_V6)
 
 # add fake ip addresses
-root.append(Node(IPv4Network('198.18.0.0/16')))
+# root.append(Node(IPv4Network('198.18.0.0/16')))
 
 with open("routes4.conf", "w") as f:
     dump_bird(root, f)
